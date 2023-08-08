53078f1bd49c7471e682187950cbfc7121f707a8
akani28 <nietocevallos61@gmail.com>
2023-08-08 02:09:46 -0500

    Create folder components and  jxs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+import Weather from './components/Weather'
 
 function App() {
+  const [weatherInfo, setWeatherInfo] = useState(null)
 
   const success = (pos) =>{
     const lat = pos.coords.latitude
     const lon = pos.coords.longitude
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={API key}`
+    const API_KEY = "00f358c88d7bb99b4dd87d18d444a5b4"
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+
+    axios.get(url)
+    .then(({data})=>setWeatherInfo(data))
+    .catch((err)=>console.log(err))
 
   }
   useEffect(()=>{
@@ -17,8 +26,9 @@ function App() {
  
 
   return (
-    <main className='bg-black min-h-screen text-white'>
-      <h2>weather</h2>
+    <main className='bg-black min-h-screen text-white font-lato'>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, voluptatem?</h2>
+      <Weather weatherInfo={weatherInfo}/>
    
     </main>
   )
---
+++ src/components/Weather.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Weather = ({weatherInfo}) => {
+  return (
+    <div>Algo</div>
+  )
+}
+
+export default Weather
